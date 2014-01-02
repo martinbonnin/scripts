5823f767eea85d0925faa9ac886e6133046febce
Martin Bonnin <martin@mbonnin.net>
2014-01-02 17:23:53 +0100

    add support for DirectoryIndex using RewriteRule
    improve with slash redirect

--- apache/intercept.sh
+++ apache/intercept.sh
@@ -1,30 +1,34 @@
 #! /bin/bash
 
-MBONNIN_DIR=/home/martin/git/mbonnin.net
+MBONNIN_DIR=/usr/local/home/martin/git/mbonnin.net
 cd ${MBONNIN_DIR}
 
-FILE=${MBONNIN_DIR}/site/${PATH_REQUESTED}
+TMP_DIR=/usr/local/home/martin/apache/mbonnin.net
+FILE=${TMP_DIR}/${PATH_REQUESTED}
+
+case $FILE in 
+    *.html ) TYPE="text/html" ;;
+    *.css ) TYPE="text/css" ;;
+    * ) TYPE="text/plain" ;;
+esac
+
+if [ $TYPE == "text/html" ]
+then
+    ./generator/generator.py skeleton ${TMP_DIR} >/tmp/generator.error 2>&1 
+
+    if [ $? -ne 0 ]
+    then
+        echo -e -n "Content-type: text/html\n";
+        echo -e -n "Status: 404\n\n"
+        echo "<code style=\"white-space: pre;\">"
+        cat /tmp/generator.error
+        echo "</code>"
+        exit 1
+    fi
+fi
+
 if [ -f ${FILE} ]
 then
-    case $FILE in 
-        *.html ) TYPE="text/html" ;;
-        *.css ) TYPE="text/css" ;;
-        * ) TYPE="text/plain" ;;
-    esac
-    
-    if [ $TYPE == "text/html" ]
-    then
-    ./generator/generator.py src site >/tmp/generator.error 2>&1 
-
-        if [ $? -ne 0 ]
-        then
-            echo -e -n "Content-type: text/html\n";
-            echo -e -n "Status: 404\n\n"
-            cat /tmp/generator.error
-            exit 1
-        fi
-    fi
-
     echo -e -n "Content-type: ${TYPE}\n";
     echo -e -n "Status: 200\n\n"
     cat ${FILE}
