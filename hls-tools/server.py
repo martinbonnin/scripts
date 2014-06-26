2e9924186ea84577079b67b5d669ef54195d4b4b
martinbonnin <martin@mbonnin.net>
2014-06-26 11:48:11 -0700

    patch TS files on the fly by adding stuffing packets and removing discontinuity flags

--- hls-tools/server.py
+++ hls-tools/server.py
@@ -12,12 +12,75 @@ import sys
 
 PORT = 8080
 
+def patch_ts(fd):
+	streams = {}
+	output = bytearray(0)
+	print('patch!')
+	def log(m):
+		if (False):
+			print(m)
+
+	while(True):
+		packet = bytearray(fd.read(188))
+		if (len(packet) != 188):
+			break
+		if (packet[0] != 0x47):
+			print('bad sync byte: %x'%packet[0])
+		pid = (packet[1] & 0x1f) << 8
+		pid |= packet[2]
+		cc_counter = packet[3] & 0xf
+		
+		if (not pid in streams):
+			log('new pid = 0x%04X' % pid)
+			# I substract 1 from cc_counter so that we do not have a warning for the first packet
+			streams[pid] = {'cc_counter': (cc_counter - 1) & 0xf, 'packet_count': 0}
+		stream = streams[pid]
+
+		adaptation_field_exists = packet[3] & 0x20
+		prefix_size = 4
+		if (adaptation_field_exists):
+			if (packet[prefix_size + 1] & 0x80):
+				log('discontinuity bit is set on pid %04x, packet %d, clear' % (pid, stream['packet_count']))
+				packet[prefix_size + 1] &= ~0x80
+		
+		expected_cc_counter = (stream['cc_counter'] + 1) & 0xf
+		#print('%04x: %2d' % (pid, cc_counter))
+		if (cc_counter != expected_cc_counter):
+			print('cc error: %2d -> %2d' % (stream['cc_counter'], cc_counter))
+		stream['cc_counter'] = cc_counter
+		stream['packet_count'] += 1
+		output.extend(packet)
+
+	for pid in streams:
+		stream = streams[pid]
+		log('pid 0x%04x last_cc_counter: %2d' % (pid, stream['cc_counter']))
+		# do not patch PAT, SDT or PMT
+		if (pid == 0 or pid == 0x11 or pid == 0x100):
+			continue
+		cc_counter = stream['cc_counter']
+		while (cc_counter != 0xf):
+			cc_counter += 1
+			packet = bytearray(188)
+			packet[0] = 0x47
+			packet[1] = pid >> 8
+			packet[2] = pid & 0xff
+			packet[3] = 0x20 | cc_counter #adaptation field exists, no payload
+			packet[prefix_size] = 183
+			packet[prefix_size + 1] = 0
+			log('stuff...')
+			output.extend(packet)
+			#stuffing
+			
+
+	return output
+	
+		
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	
 	def do_GET(self):
-		print("got request for " + self.path);
-		#pdb.set_trace();
-		path = hls_dir + self.path;
+		print("got request for " + self.path)
+		#pdb.set_trace()
+		path = hls_dir + self.path
 				
 		try:
 			fd = open(path);
@@ -33,16 +96,19 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 			else:
 				t =  'application/octet-stream'
 
-			data = fd.read();
-			self.send_response(200);
-			self.send_header("Content-type", t);
-			self.send_header("Content-length", len(data));
-			self.end_headers();
-			self.wfile.write(data);
+			if (ext == '.ts'):
+				data = patch_ts(fd)
+			else:
+				data = fd.read()
+			self.send_response(200)
+			self.send_header("Content-type", t)
+			self.send_header("Content-length", len(data))
+			self.end_headers()
+			self.wfile.write(data)
 		else:
-			self.send_response(404);
-			self.end_headers();
-			self.wfile.write("cannot find " + path + "\n");
+			self.send_response(404)
+			self.end_headers()
+			self.wfile.write("cannot find " + path + "\n")
 		
 class MyServer(SocketServer.TCPServer):
 	allow_reuse_address = True
